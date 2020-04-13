85d8d9bc3cbfe6cf0043e81656ca099b3f20599d
Вадим Раковец <r.vadim.90@gmail.com>
2020-04-14 02:30:54 +0300

    error correction

---
+++ RemoteSoungs.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace clientSoung
+{
+    partial class RemoteSoungs
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "192.168.100.12";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(245, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "65535";
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(76, 182);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(209, 182);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(418, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Client remote soungs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RemoteSoungs.cs
@@ -0,0 +1,82 @@
+﻿using NAudio.Wave;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace clientSoung
+{
+    public partial class RemoteSoungs : Form
+    {
+        private bool play = false;
+        WaveOut outSoung;
+        BufferedWaveProvider buffer;
+        Thread thread;
+        Socket listeningSocket;
+        public RemoteSoungs()
+        {
+            InitializeComponent();
+        }
+        private void Listening()
+        {
+            string Host = Dns.GetHostName();
+            EndPoint remoteIp = new IPEndPoint(IPAddress.Parse(Dns.GetHostByName(Host).AddressList[0].ToString()), 8855);
+            int siseArr = int.Parse(textBox2.Text);
+            listeningSocket.Bind(remoteIp);
+            while (play)
+            {
+                try
+                {
+                    byte[] data = new byte[siseArr];
+                    int received = listeningSocket.ReceiveFrom(data, ref remoteIp);
+                    buffer.AddSamples(data, 0, received);
+                }
+                catch (SocketException ex)
+                {
+                    play = false;
+                    MessageBox.Show(ex.Message);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    play = false;
+                    MessageBox.Show(ex.Message);
+                    break;
+                }
+            }
+            thread = new Thread(new ThreadStart(Listening));
+            thread.Start();
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (!play)
+            {
+                buffer = new BufferedWaveProvider(new WaveFormat(36000, 32, 2));
+                outSoung = new WaveOut();
+                outSoung.Init(buffer);
+                outSoung.Play();
+                play = true;
+                listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                thread = new Thread(new ThreadStart(Listening));
+                thread.Start();
+            }
+        }
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (play)
+            {
+                play = false;
+                listeningSocket.Close();
+                listeningSocket.Dispose();
+                if (outSoung != null)
+                {
+                    outSoung.Stop();
+                    outSoung.Dispose();
+                    outSoung = null;
+                }
+                buffer = null;
+            }
+        }
+    }
+}
